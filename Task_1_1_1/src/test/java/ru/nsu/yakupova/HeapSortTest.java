cf6a7f29dbc6967bd1d82ad815187b50c17eb8c5
Alina Yakupova <a.yakupova2004@yandex.ru>
2023-09-13 12:55:46 +0700

    changes for import in test file (wrong order)

--- Task_1_1_1/src/test/java/ru/nsu/yakupova/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/yakupova/HeapSortTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.yakupova;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
