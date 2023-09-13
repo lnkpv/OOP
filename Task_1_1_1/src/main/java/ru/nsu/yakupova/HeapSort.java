e75bedb0adf087631c8c1578cfa22ff45ff8a0af
Alina Yakupova <a.yakupova2004@yandex.ru>
2023-09-13 11:02:23 +0700

    solution for t1

---
+++ Task_1_1_1/src/main/java/ru/nsu/yakupova/HeapSort.java
@@ -0,0 +1,60 @@
+package ru.nsu.yakupova;
+
+
+public class HeapSort
+{
+    public static void heapsort(int[] arr)
+    {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i=n-1; i>=0; i--)
+        {
+            int cur_root = arr[0];
+            arr[0] = arr[i];
+            arr[i] = cur_root;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    static public void heapify(int[] arr, int n, int i)
+    {
+        int root = i;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+
+        if (l < n && arr[l] > arr[root])
+            root = l;
+
+        if (r < n && arr[r] > arr[root])
+            root = r;
+
+        if (root != i)
+        {
+            int tmp = arr[i];
+            arr[i] = arr[root];
+            arr[root] = tmp;
+
+            heapify(arr, n, root);
+        }
+    }
+
+    static void printArray(int[] arr)
+    {
+        int n = arr.length;
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {5, 4, 3, 2, 1};
+
+        heapsort(arr);
+
+        printArray(arr);
+    }
+}
---
+++ Task_1_1_1/src/test/java/ru/nsu/yakupova/HeapSortTest.java
@@ -0,0 +1,23 @@
+package ru.nsu.yakupova;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTest {
+
+    @Test
+    void checkMain() {
+        HeapSort.main(new String[] {});
+        assertTrue(true);
+    }
+
+    @Test
+    void testDefaultArray(){
+//        HeapSort ob = new HeapSort();
+        int[] sampleArr = new int[] {5,4,3,2,1};
+        HeapSort.heapsort(sampleArr);
+        int[] arr = new int[] {1,2,3,4,5};
+        assertArrayEquals(arr, sampleArr);
+    }
+}
