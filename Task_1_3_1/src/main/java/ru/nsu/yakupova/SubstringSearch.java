16935cdb871c3eab67aeb1413d9102d920211fd8
Alina Yakupova <a.yakupova2004@yandex.ru>
2023-11-07 19:05:17 +0700

    reviewdog 2

--- Task_1_3_1/src/main/java/ru/nsu/yakupova/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/yakupova/SubstringSearch.java
@@ -1,8 +1,8 @@
 package ru.nsu.yakupova;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
