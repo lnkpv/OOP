7439a10e6dfd4c561d8c258c8ec455c4427e0809
lnkpv <lnkpv@users.noreply.github.com>
2023-11-22 15:41:11 +0000

    Deploying to gh-pages from @ lnkpv/OOP@3d400979d5140b6db10b0aff12e581c44de15ebe 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.yakupova","l":"Student"},{"p":"ru.nsu.yakupova","l":"StudentsBook"},{"p":"ru.nsu.yakupova","l":"Subject"}]
